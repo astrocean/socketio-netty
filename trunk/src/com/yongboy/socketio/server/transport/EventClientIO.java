dc2c8ceed9a4bf4f634be925aac85b1579271052
yongboy@gmail.com <yongboy@gmail.com@221c7d64-1d88-f561-2dca-68f723b4e2a3>
2012-04-05 10:01:18 +0000

    remove the events's method

--- trunk/src/com/yongboy/socketio/server/transport/EventClientIO.java
+++ trunk/src/com/yongboy/socketio/server/transport/EventClientIO.java
@@ -1,12 +1,7 @@
 package com.yongboy.socketio.server.transport;
 
-import java.util.Collections;
 import java.util.HashMap;
-import java.util.HashSet;
 import java.util.Map;
-import java.util.Set;
-
-import com.yongboy.socketio.server.SocketIOManager;
 
 /**
  * 
@@ -15,34 +10,9 @@ import com.yongboy.socketio.server.SocketIOManager;
  * @version 1.0
  */
 abstract class EventClientIO implements IOClient {
-	private Set<String> events = null;
 	public Map<String, Object> attr = null;
 
 	public EventClientIO() {
-		events = new HashSet<String>();
 		attr = new HashMap<String, Object>();
-	}
-
-	public void addEvent(String event) {
-		if (SocketIOManager.fobbiddenEvents.contains(event))
-			return;
-
-		events.add(event);
-	}
-
-	public void removeEvent(String event) {
-		events.remove(event);
-	}
-
-	public Set<String> getEvents() {
-		return Collections.unmodifiableSet(events);
-	}
-
-	public boolean containsEvent(String event) {
-		return events.contains(event);
-	}
-
-	public void clearEvents() {
-		events.clear();
 	}
 }
